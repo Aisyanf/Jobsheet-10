8f2fe0d96d6e7f8eafe6d46ce563b00b2e1e534a
Aisyanf <an.fardani@gmail.com>
2025-11-13 09:04:36 +0700

    Percobaan 1-Pertanyaan 5

--- Bioskop02.java
+++ Bioskop02.java
@@ -16,11 +16,11 @@ public class Bioskop02 {
         System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
 
-        System.out.println(penonton.length);
-        System.out.println(penonton[0].length);
-        System.out.println(penonton[1].length);
-        System.out.println(penonton[2].length);
-        System.out.println(penonton[3].length);
+        System.out.println("\n" +penonton.length);
+
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Panjang baris ke-" +(i+1) +" : " +penonton[1].length);
+        }
 
     }
 }
